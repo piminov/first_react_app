259cc8c8142e7993706a0ec658a5adc184e7ca69
piminov <dima.piminov@gmail.com>
2022-06-08 16:59:31 +0800

    add route module and routes lection 19

--- src/App.js
+++ src/App.js
@@ -5,19 +5,26 @@ import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 
 const App = () => {
 
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-            {/*<Profile />*/}
-            <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+
+                <div className='app-wrapper-content'>
+                    <Routes>
+                        <Route path='/dialogs' element={<Dialogs/>}/>
+                        <Route path='/profile' element={<Profile/>}/>
+                    </Routes>
+                    <Dialogs/>
+                </div>
             </div>
-        </div>
+        </BrowserRouter>
     );
 }
 
